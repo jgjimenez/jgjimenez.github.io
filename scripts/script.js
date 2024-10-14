289e915768e21eaa2ba8f7b45c991ec30852851f
Jose Gregorio Jimenez Sanchez <josegjimenez@gmail.com>
2024-10-14 18:56:12 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-// Desplazamiento personalizado en el carrusel si necesitas controlar el movimiento.
+
 function moveSlide(step) {
     let carousel = new bootstrap.Carousel(document.querySelector('#carouselLogos'));
     if (step === -1) {
