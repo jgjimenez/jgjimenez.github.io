8157e11ad910b0ec99f7a837f53f13b9a4c2b67d
Jose Gregorio Jimenez Sanchez <josegjimenez@gmail.com>
2024-10-29 17:35:39 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -102,7 +102,7 @@ document.addEventListener('DOMContentLoaded', function() {
 const donationGoal = 15000; 
 
 
-let currentDonations = 116; 
+let currentDonations = 166; 
 
 
 function updateProgress() {
