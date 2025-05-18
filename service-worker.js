6b9d51b4dbf42976977e5125d9411367aea23c0a
Jose Gregorio Jimenez Sanchez <josegjimenez@gmail.com>
2025-05-18 15:11:15 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -20,4 +20,15 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
