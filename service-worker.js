fee19f205d018a99bc2d70bfb64077121592a68f
Jose Gregorio Jimenez Sanchez <josegjimenez@gmail.com>
2025-05-18 19:36:24 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,53 +1,53 @@
-const CACHE_NAME = 'radio-vida-eterna-cache-v6';
+const CACHE_NAME = 'radio-vida-eterna-cache-v2';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/info.html',
-  '/donacion.html',
-  '/cronograma.html',
   '/css/styles.css',
   '/script/script.js',
-  '/images/Radio_Vida_Eterna_logo.png',
   '/images/Radio_Vida_Eterna_logo_192.png',
-  '/images/Radio_Vida_Eterna_logo_512.png',
-  '/images/fondo.jpg',
-  '/images/logo.png'
+  '/images/Radio_Vida_Eterna_logo_512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then((cache) => {
+        console.log('Cache abierto:', CACHE_NAME);
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', (event) => {
+  // Excluir el stream de audio del caché
   if (event.request.url.includes('conectarhosting.com/8414/stream')) {
     return fetch(event.request);
   }
   
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request)
-        .then(fetchResponse => {
-          const responseClone = fetchResponse.clone();
-          caches.open(CACHE_NAME)
-            .then(cache => cache.put(event.request, responseClone));
-          return fetchResponse;
-        })
-        .catch(() => caches.match('/index.html'))
-      )
+      .then((response) => {
+        return response || fetch(event.request)
+          .then(fetchResponse => {
+            return caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request.url, fetchResponse.clone());
+              return fetchResponse;
+            });
+          });
+      })
   );
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => 
-      Promise.all(
-        cacheNames.map(cacheName => 
-          cacheName !== CACHE_NAME ? caches.delete(cacheName) : null
-        )
-      )
-    )
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
